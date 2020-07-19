b5a54716d1a66bbda58bc20ea552380833d98dcc
erasebegin <chris.james.haupt@gmail.com>
2020-07-19 11:34:17 +0100

    first commit

---
+++ src/templates/blog-post.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import Img from 'gatsby-image'
+
+import heroStyles from '../components/hero.module.css'
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost')
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+
+    return (
+      <div style={{ background: '#fff' }}>
+        <Helmet title={`${post.title} | ${siteTitle}`} />
+        <div className="wrapper">
+          <h1 className="section-headline">{post.title}</h1>
+          <p
+            style={{
+              display: 'block',
+            }}
+          >
+            {post.publishDate}
+          </p>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.body.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
